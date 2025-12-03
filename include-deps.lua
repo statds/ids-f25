8bdddcdebcc43120d426dffd15e939817192adbd
Jun Yan <1536656+jun-yan@users.noreply.github.com>
2025-12-03 18:49:03 -0500

    removed the lua trick because it did not work

--- include-deps.lua
+++
@@ -1,9 +0,0 @@
--- include-deps.lua
-function RawBlock(el)
-  -- Match {{ include filename }}
-  local inc = el.text:match("{{%s*include%s+([^%s}]+)%s*}}")
-  if inc then
-    quarto.doc.add_dependency(inc)
-  end
-  return el
-end
